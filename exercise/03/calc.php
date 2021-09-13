f5e0faf8345776249b054d7ab746a08d30a0516d
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-13 19:20:38 +0900

    空白の修正

---
+++ exercise/03/calc.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = 3;
+$num2 = 4;
+$operator = 'addition';
+
+?>
+
+<!DOCTYPE html>
---
+++ exercise/06/main.php
@@ -0,0 +1,12 @@
+<?php
+
+class Car
+{
+    public function __construct()
+    {
+    }
+}
+
+$user = new Car();
+
+var_dump($car);
