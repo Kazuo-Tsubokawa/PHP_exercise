d93208fb23dccc441d683fff6fe2114af1cf5c4a
Kazuo-Tsubokawa <hiwa1712@gmail.com>
2021-09-14 12:30:51 +0900

    オブジェクト指向の課題完成

--- exercise/06/Car.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Car
-{
-    public function __construct()
-    {
-    }
-
-}
--- exercise/06/index.php
+++ exercise/06/index.php
@@ -2,12 +2,54 @@
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
 
+class Car
+{   public $name;
+    public $number;
+    public $color;
 
+    public function __construct($name, $number, $color)
+    {
+        $this->name = $name;
+        $this->number = $number;
+        $this->color = $color;
+    }
 
+    public function getName() 
+    {   
+        return $this->name;
+    }
 
-// 以下を実行した時にエラーがでないようにして下さい。
-// Carクラスのインスタンスを生成
-// 引数 : 車名、車体番号、カラー
+    public function getNumber() 
+    {   
+        return $this->number;
+    }
+
+    public function getColor() 
+    {   
+        return $this->color;
+    }
+
+    public function information()
+    {
+        return '車の車種:' . $this->name . '、車体番号:' . $this->number . '、カラー:' . $this->color . 'です。';
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setNumber($number)
+    {
+        $this->name = $number;
+    }
+
+    public function setColor($color)
+    {
+        $this->name = $color;
+    }
+} 
+
 $car1 = new Car('ランクル', 100, 'black');
 
 // 車名(車種)を取得
